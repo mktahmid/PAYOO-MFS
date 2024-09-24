0852a5b8611a348cfc1cbc074f97e39256c218ce
tahmid <mktahmid4@gamil.com>
2024-09-24 19:55:00 +0600

    added money btn

--- js/home.js
+++ js/home.js
@@ -1,1 +1,17 @@
-console.log("added");
+// add money to the account
+
+// setp-1: add an event handler to the add money button inside the form and prevent page reload after form submit
+// setp-2: get money to be the acount balance
+
+// setp-1: add an event handler to the add money button inside the form  
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+  // prevent page reload after form submit
+  event.preventDefault()
+
+  // setp-2: get money to be the acount balance
+  const addMoneyInput = document.getElementById('input-add-money').value
+  console.log(addMoneyInput);
+  // get the pin number provided
+  const pinNumberInput = document.getElementById('input-add-pin').value
+  console.log(pinNumberInput);
+})
